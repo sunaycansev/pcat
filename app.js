d9f946952491432afd251a752aa6a4be922da820
sunaycansev <fsunaycansev@gmail.com>
2022-01-04 01:23:01 +0300

    add ejs files

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
+
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
 
+// Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
 const port = 3000;
